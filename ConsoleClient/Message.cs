b53c42cd39dfa9092cc09e1673c3f1852eda485f
Acer <Acer@DESKTOP-I2DLBMU>
2023-04-30 20:35:31 +0300

    Serialization and Deserialization

--- ConsoleClient/Message.cs
+++ ConsoleClient/Message.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyMessanger
 {
+    [Serializable]
     public class Message
     {
         public string UserName { get; set; }
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,12 +1,18 @@
-﻿ namespace MyMessanger
+﻿using Newtonsoft.Json;
+
+namespace MyMessanger
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine(msg.ToString());
-            Console.WriteLine("Hello, World!");
+            Message msg = new Message("Iambakhh", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //Console.WriteLine(msg.ToString());
+            //Console.WriteLine("Hello, World!");
         }
     }
 }
